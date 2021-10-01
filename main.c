c8de5874f1ba78bb9db3dd2cdb178b02bfe39653
Gabriel Z. Salvi <gabriel.z.salvi@gmail.com>
2021-10-01 11:24:29 -0300

    implemented the creation and insertion of a task in a doubly linked list

--- main.c
+++ main.c
@@ -12,25 +12,28 @@
 #include <stdio.h> 
 #include <stdlib.h>
 
-#define EXIT 10  // valor fixo para a opção que finaliza a aplicação
+#define EXIT 10
 
-//Struct que representa a data.
 typedef struct {
 	int day;
 	int month;
 } Date;
 
-// Estrutura que contém os campos dos registros das tarefas
 struct REC {
     char nome[50];
     int prioridade;
     Date entrega; 
-    struct REC *next; // implemente como lista, como árvore BST, AVL...
+    struct REC *next;
     struct REC *prev;
 };
 
-// Tipo criado para instanciar variaveis do tipo acima
 typedef struct REC Task;
+
+typedef struct
+{
+    Task *head;
+    Task *tail;
+} ToDoList;
 
 // Apresenta o menu da aplicação e retorna a opção selecionada
 int menu()
@@ -41,37 +44,84 @@ int menu()
         printf("%d Finaliza",EXIT);
         printf("\n: ");
         scanf("%d",&op);
-       
     }
+
     return op;
 }
 
-// Permite o cadastro de uma tarefa
-void insTask()
+void initToDoList(ToDoList *toDoList)
 {
-    return;
+    toDoList->head = NULL;
+    toDoList->tail = NULL;
+}
+
+Task *createTask() 
+{
+    Task *task = (Task *)malloc(sizeof(Task));
+
+    printf("\nDigite os dados da nova tarefa:\n");
+
+    printf("Nome: ");
+    scanf("%s", task->nome);
+
+    printf("Prioridade: ");
+    scanf("%d", &task->prioridade);
+
+    printf("Entrega (dd/MM): ");
+    scanf("%d/%d", &task->entrega.day, &task->entrega.month);
+
+    task->prev = NULL;
+    task->next = NULL;
+
+    return task;
+}
+
+// Permite o cadastro de uma tarefa
+void insTask(ToDoList *tasksList)
+{
+    Task *task = createTask();
+
+    if (task == NULL) {
+        return;
+    }
+
+    // se não houver nenhuma tarefa registrada ainda
+    if (tasksList->head == NULL) 
+    {
+        tasksList->head = task;
+        tasksList->tail = task;
+    }
+    else 
+    {
+        task->prev = tasksList->tail;
+        tasksList->tail = task;
+    }
+
+    printf("\nTarefa adicionada!\n");
 }
 
 // Permite excluir uma tarefa
-void delTask ()
+void delTask()
 {
     return;
 }
 
 // Lista o conteudo da lista de tarefas (todos os campos)
-void listTasks ()
+void listTasks()
 {
+    printf("\nLista de Tarefas\n----------------\n\n");
+
     return;
 }
 
 // Permite consultar uma tarefa da lista pelo nome
-void querytask ()
+void querytask()
 {
     return;
 }
 
 // Permite a atualização dos dados de uma tarefa
-void upTask ()
+void upTask()
 {
     return;
 }
@@ -79,15 +129,17 @@ void upTask ()
 // Programa principal
 int main()
 {
-    int op = 0;
-    Task t;
+    ToDoList toDoList;
+    initToDoList(&toDoList);
 
+    int op = 0;
+    
     while (op!=EXIT)
     {
         op = menu();
         switch(op)
         {
-            case 1 : insTask();
+            case 1 : insTask(&toDoList);
             case 2 : delTask();
             case 3 : upTask();
             case 4 : queryTask();
